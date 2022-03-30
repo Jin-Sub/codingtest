a9f43ad1cabb0fe5c87055651f76147a35633890
jinsy <Jinsyz95@gmail.com>
2022-03-30 18:58:13 +0900

    기본수학

---
+++ 기본수학/1193.py
@@ -0,0 +1,14 @@
+N = int(input())
+line = 1
+while N > line:
+    N -= line
+    line += 1
+
+if line % 2 == 0:
+    a = N
+    b = line + 1 - a
+else:
+    b = N
+    a = line + 1 - b
+
+print(a, '/', b, sep='')
